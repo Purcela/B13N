4f020e8f772239f7d8ec9ba0cb4964f447986ddc
Giorgi phurtseladze <99269771+Purcela@users.noreply.github.com>
2023-09-12 19:36:43 +0400

    gallery-start

--- js/script.js
+++ js/script.js
@@ -157,29 +157,40 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "You selected: " + selectedCategory;
 }
 
-// ------------------------------- search ------------------------------
-
-function searchByID() {
-    var input, filter, productBoxes, idBoxes, i;
-    input = document.getElementById('searchInput');
-    filter = input.value.trim().toUpperCase();
-    productBoxes = document.querySelectorAll('.product-box');
-    idBoxes = document.querySelectorAll('.id-box'); // Corrected class name
-
-    for (i = 0; i < productBoxes.length; i++) {
-        var searchText = idBoxes[i].textContent.trim().toUpperCase();
-        if (searchText.indexOf(filter) > -1) {
-            productBoxes[i].style.display = 'block';
-        } else {
-            productBoxes[i].style.display = 'none';
-        }
-    }
-}
 //---------------------------- gallery view --------------------------------
 
+const images = document.querySelectorAll('[data-enlargable]');
+const modal = document.querySelector('.image-modal');
+const modalImg = document.getElementById('zoomed-img');
+const closeBtn = document.querySelector('.close-modal');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentIndex = 0;
 
+images.forEach((image, index) => {
+    image.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = image.src;
+        currentIndex = index;
+    });
+});
 
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
 
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        modalImg.src = images[currentIndex].src;
+    }
+});
 
-// ------------------------------ testing -------------------------------------
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+        modalImg.src = images[currentIndex].src;
+    }
+});
+
 
